8c5ebf5f54bf1bf0840f5cfc83ec959b03478a02
Nicolas Estevez <nicolas.estevez@pressganey.com>
2023-06-07 21:41:49 -0300

    using api from zenRow

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dataUtils = require('./data-utils');
 (async () => {
      console.time("Scrapper");
      // await core.locationScrap('cordoba')
-     await core.realScrapApi('https://clasificados.lavoz.com.ar/inmuebles/terrenos-y-lotes/venta', 'la-voz-try', 1)
+     await core.realScrapApi('https://clasificados.lavoz.com.ar/inmuebles/terrenos-y-lotes/venta', 'la-voz-try', 5)
      // await core.fsWriteTry('la-voz-try', 1, [{id: 1, message: 'Prueba nico'}])
      // await core.neighborhoodScrap('#input-ubicacion','cordoba')
      // console.table(await utils.findWordsByProximity('terreno de 360 m2 en frente norte', 'f norte', 5))
